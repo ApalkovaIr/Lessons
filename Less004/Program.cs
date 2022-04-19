840ba806d7e25a7ab24df80e4085e69f84203a8c
AplkovaIrina <missis.tim@gmail.com>
2022-04-19 16:08:13 +0300

    Add Less04

---
+++ Less004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Принимаем, что работаем с целыми числами
+int N = Console.Read(); 
+int a = 1;
+int i = 1;
+while (true)
+{
+    if(a % 2 == 0)
+    {
+      System.Console.WriteLine("a");
+      i = i + 1;
+    }
+    if (i > N )
+    { 
+        break;
+    }
+}
