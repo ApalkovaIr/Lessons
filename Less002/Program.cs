df8de7bc28a52023ecb42afc94d3872aba781901
AplkovaIrina <missis.tim@gmail.com>
2022-04-19 15:10:50 +0300

    Add Less002

---
+++ Less002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принимаем, что заказчик работает с рублями без копеек
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+{
+if (b > max)
+{   
+     Console.WriteLine("max = b");
+}
+     else
+if (c > max)
+     Console.WriteLine("max = c");           
+}
